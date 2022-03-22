1ce12a37de135ef7aee631f9290f8115164317c8
josephchancey <josephchancey11@gmail.com>
2022-03-22 16:08:20 -0700

    GPG error handling

--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
-# e-Learning Platform API
+# eLearning Platform API
+This node/express.js API is built to represent the backend of a coding bootcamp management system.
+
+## Background
+This project was inspired by the UC Davis' Data Analytics bootcamp I graduated from in 2021.
+
+## Repo Structure
+
+## Endpoints
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,32 +1,26 @@
-const express = require('express');
+const express = require("express");
+
+// Use destructuring to bring in functions from controller
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
+
 const router = express.Router();
 
+router
+    .route("/")
+    .get(getBootcamps)
+    .post(createBootcamp);
+
+router
+    .route('/:id')
+    .get(getBootcamp)
+    .put(updateBootcamp)
+    .delete(deleteBootcamp);
 
 
-// Route to get all bootcamps
-router.get('/', (req, res) => {
-    res.status(200).json({ success: true, msg: "Show all" });
-});
-
-// Route to get specific bootcamp by id
-router.get('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
-});
-
-// Route to create bootcamp
-router.post('/', (req, res) => {
-    res.status(200).json({ success: true, msg: "Create new" });
-});
-
-// Route to update bootcamp
-router.put('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-// Route to delete bootcamp
-router.delete('/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
-
-// Export these routes
 module.exports = router;
